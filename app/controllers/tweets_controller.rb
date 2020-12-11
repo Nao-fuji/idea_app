006cfe737ce42d750254a57ba3fb87e459f27001
Nao-fuji <fujitaninaoya@MacBook-Pro.local>
2020-12-11 15:51:17 +0900

    画像投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,12 @@ class TweetsController < ApplicationController
     redirect_to "/recruits/#{@tweet.recruit.id}"
   end
 
+  def destroy
+    @tweet=Tweet.find(params[:id])
+    @tweet.destroy if current_user.id == @tweet.user_id
+    redirect_to "/recruits/#{@tweet.recruit.id}"
+  end 
+
   private
     def tweet_params
       params.require(:tweet).permit(:idea, :explain, :other, :image).merge(user_id: current_user.id, recruit_id: params[:recruit_id])
