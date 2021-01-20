4ab10e6751fcb57937a62a32287170480cc52c57
Nao-fuji <fujitaninaoya@MacBook-Pro.local>
2021-01-20 21:03:10 +0900

    画像のプレビュー機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../idea_purchase_card")
 require("../tweet_purchase_card")
+require('./preview')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
